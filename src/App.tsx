b8ff0d937686cba1891d85b6f08d4ab1e9793634
Valdery Alves <valdery.jur@gmail.com>
2025-08-07 10:52:56 -0300

    feat: configures project and tailwind
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import timelineItems from "./timelineItems";
+
+function App() {
+  return (
+    <div>
+      <h2>Good luck with your assignment! {"\u2728"}</h2>
+      <h3>{timelineItems.length} timeline items to render</h3>
+    </div>
+  );
+}
+
+export default App;
